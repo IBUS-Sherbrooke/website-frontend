42d3616b0e466290028d548289a9ed3016f414cb
tobiedesgreniers <tobiedesgreniers300@gmail.com>
2021-01-27 13:53:11 -0500

    Render Window functioning without data

--- src/app/components/coronal-visualisation/coronal-visualisation.component.ts
+++ src/app/components/coronal-visualisation/coronal-visualisation.component.ts
@@ -1,21 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import vtkImageSlice from 'vtk.js/Sources/Rendering/Core/ImageSlice';
+import { ViewChild, ElementRef, Component, OnInit } from '@angular/core';
+
+import vtkActor from 'vtk.js/Sources/Rendering/Core/Actor';
+import vtkMapper from 'vtk.js/Sources/Rendering/Core/Mapper';
+import vtkOpenGLRenderWindow from 'vtk.js/Sources/Rendering/OpenGL/RenderWindow';
 import vtkRenderWindow from 'vtk.js/Sources/Rendering/Core/RenderWindow';
-import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
+import vtkRenderWindowInteractor from 'vtk.js/Sources/Rendering/Core/RenderWindowInteractor';
+import vtkRenderer from 'vtk.js/Sources/Rendering/Core/Renderer';
 
 @Component({
   selector: 'app-coronal-visualisation',
   templateUrl: './coronal-visualisation.component.html',
   styleUrls: ['./coronal-visualisation.component.css']
 })
-export class CoronalVisualisationComponent implements OnInit {
+export class CoronalVisualisationComponent implements OnInit{
 
   coronalRenderWindow = null;
+  coronalRenderer = null;
+
+  @ViewChild('content', {read: ElementRef}) content: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.coronalRenderWindow = vtkRenderWindow.newInstance();
+
+  }
+  
+  ngAfterViewInit(): void {
+    const renderWindow = vtkRenderWindow.newInstance();
+    const renderer = vtkRenderer.newInstance({ background: [0, 0, 0] });
+    renderWindow.addRenderer(renderer);
+
+    const mapper = vtkMapper.newInstance();
+    const actor = vtkActor.newInstance();
+
+    actor.setMapper(mapper);
+    renderer.addActor(actor);
+    renderer.resetCamera();
+
+    const openglRenderWindow = vtkOpenGLRenderWindow.newInstance();
+    renderWindow.addView(openglRenderWindow);
+
+    openglRenderWindow.setContainer(this.content.nativeElement);
+
+    // ----------------------------------------------------------------------------
+    // Capture size of the container and set it to the renderWindow
+    // ----------------------------------------------------------------------------
+    const { width, height } = this.content.nativeElement.getBoundingClientRect();
+    openglRenderWindow.setSize(width, height);
+
+    // ----------------------------------------------------------------------------
+    // Setup an interactor to handle mouse events
+    // ----------------------------------------------------------------------------
+    const interactor = vtkRenderWindowInteractor.newInstance();
+    interactor.setView(openglRenderWindow);
+    interactor.initialize();
+    interactor.bindEvents(this.content.nativeElement);
   }
 
 }
