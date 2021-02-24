d00d85f7ec572082ef40fa136bf676b7183f6528
Michael <micha.samson@live.ca>
2021-02-24 14:01:13 -0500

    Added brightness contraste and depth

--- src/app/components/coronal-visualisation/coronal-visualisation.component.ts
+++ src/app/components/coronal-visualisation/coronal-visualisation.component.ts
@@ -8,7 +8,6 @@ import vtkOpenGLRenderWindow from 'vtk.js/Sources/Rendering/OpenGL/RenderWindow'
 import vtkRenderWindow from 'vtk.js/Sources/Rendering/Core/RenderWindow';
 import vtkRenderWindowInteractor from 'vtk.js/Sources/Rendering/Core/RenderWindowInteractor';
 import vtkInteractorStyleImage from 'vtk.js/Sources/Interaction/Style/InteractorStyleImage';
-import vtkInteractorStyleTrackballCamera from 'vtk.js/Sources/Interaction/Style/InteractorStyleTrackballCamera';
 import vtkRenderer from 'vtk.js/Sources/Rendering/Core/Renderer';
 import Constants from 'vtk.js/Sources/Rendering/Core/ImageMapper/Constants';
 
@@ -61,21 +60,18 @@ export class CoronalVisualisationComponent implements OnInit{
     this.renderWindow.addRenderer(this.renderer);
 
     this.mapper = vtkImageMapper.newInstance();
-    this.mapper.setYSlice(30);
-    
-    this.actor = vtkImageSlice.newInstance();
+    this.mapper.setSliceAtFocalPoint(true);
+    this.mapper.setSlicingMode(SlicingMode.Y);
 
+    this.actor = vtkImageSlice.newInstance();
     this.actor.setMapper(this.mapper);
     this.renderer.addActor(this.actor);
-    this.camera = this.renderer.getActiveCamera();
+    this.camera = this.renderer.getActiveCamera();   
+    this.camera.setParallelProjection(true);
 
     this.camera.pitch(-90);
     this.camera.setViewUp([0, 1, 0]);
-
-    this.camera.setParallelProjection(true);
-    this.renderer.resetCameraClippingRange();
-    this.renderer.resetCamera();
-
+    
     this.openglRenderWindow = vtkOpenGLRenderWindow.newInstance();
     this.renderWindow.addView(this.openglRenderWindow);
 
@@ -95,9 +91,9 @@ export class CoronalVisualisationComponent implements OnInit{
     this.interactor.initialize();
     this.interactor.bindEvents(this.coronalDiv.nativeElement);
 
-    //this.interactor.setInteractionMode("IMAGE_SLICING");
-    //this.interactor.setInteractorStyle(vtkInteractorStyleImage.newInstance("IMAGE_SLICING"));
-    this.interactor.setInteractorStyle(vtkInteractorStyleTrackballCamera.newInstance());
+    const iStyle = vtkInteractorStyleImage.newInstance();
+    iStyle.setInteractionMode("IMAGE_SLICING");
+    this.interactor.setInteractorStyle(iStyle);
   }
 
 }
