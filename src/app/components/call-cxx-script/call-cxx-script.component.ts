fd1b1806162c7be49109cf5280ed589365246197
PhilippePoirier <Philippe.Poirier1@hotmail.com>
2021-03-21 19:09:26 -0400

    Update for full script

--- src/app/components/call-cxx-script/call-cxx-script.component.ts
+++ src/app/components/call-cxx-script/call-cxx-script.component.ts
@@ -4,8 +4,8 @@ import runPipelineBrowser from 'itk/runPipelineBrowser';
 import IOTypes from 'itk/IOTypes';
 import macro from 'vtk.js/Sources/macro';
 import ITKHelper from 'vtk.js/Sources/Common/DataModel/ITKHelper';
-
-
+import { VisualisationDataService } from "../../services/visualisation-Data/visualisation-data.service";
+import { Subscription } from 'rxjs';
 
 const { vtkErrorMacro } = macro;
 declare var require: any
@@ -16,8 +16,9 @@ declare var require: any
 })
 export class CallCxxScriptComponent implements OnInit {
   webWorker
-  
-  constructor() {     
+  img_data:any
+  subscription: Subscription;
+  constructor(private visualisationDataService: VisualisationDataService) {     
   //Load the script to call locally so that runPipelineBrowser can find it.
   const script = document.createElement("script")
   script.type = "text/javascript"
@@ -26,14 +27,24 @@ export class CallCxxScriptComponent implements OnInit {
 
   ngOnInit(): void {
     this.webWorker=null
+
+    this.subscription = this.visualisationDataService.getRawData()
+    .subscribe(imageData => {
+      this.img_data=imageData
+    }),
+    error => {
+      console.log(error);
+    }
   }
   call_script(){
 
     runPipelineBrowser(
       this.webWorker, //We pass null here, to create a new web worker
-      'HelloWorld'//Script to call
+      'HelloWorld',//Script to call
+      ['Segmentation', 'input.json', 'output.json', String(0),String(255),String(5),String(5),String(5)],//args
+      [{ path: 'output.json', type: IOTypes.Image }], // output
+      [{ path: 'input.json', type: IOTypes.Image, data: this.img_data }] // input
       ).then((result) => {
-      
       console.log(result)
       this.webWorker = result.webWorker;
       })
