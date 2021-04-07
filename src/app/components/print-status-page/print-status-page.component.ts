8d0f5bbbf664bb0bd9175e6798d5d553723f9497
PhilippePoirier <Philippe.Poirier1@hotmail.com>
2021-04-07 17:07:36 -0400

    Small bugfix
    Fixed a bug where the initial page would have the print_status page not hidden

--- src/app/components/print-status-page/print-status-page.component.ts
+++ src/app/components/print-status-page/print-status-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-print-status-page',
@@ -6,13 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./print-status-page.component.css']
 })
 export class PrintStatusPageComponent implements OnInit {
-
-  constructor() { }
+  print_status: string;
+  errorMessage: string;
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
-  send_input(text){
+  request_print_status(text){
     console.log(text)
   }
-
+  
+  get_print_status(text2){
+  this.http.get('http://localhost:2000/api/printRequests/mock', {responseType: 'text'}).subscribe({
+    next: data => {
+        this.print_status = data;
+        console.log(this.print_status)
+    },
+    error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', error);
+    }
+  })
+  }
 }
