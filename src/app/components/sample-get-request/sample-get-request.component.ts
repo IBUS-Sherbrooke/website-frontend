7845f1f3e69195256e65c662aecc6607a014c2e7
Michael <micha.samson@live.ca>
2021-04-08 10:25:24 -0400

    First

--- src/app/components/sample-get-request/sample-get-request.component.ts
+++ src/app/components/sample-get-request/sample-get-request.component.ts
@@ -6,22 +6,23 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './sample-get-request.component.html',
   styleUrls: ['./sample-get-request.component.css']
 })
+
 export class SampleGetRequestComponent implements OnInit {
-  get_string: string = "Le serveur n'est pas en train de rouler";
+  getString = 'Impossible de contacter le serveur';
   errorMessage: string;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('http://localhost:2000/api/printRequests/mock', {responseType: 'text'}).subscribe({
       next: data => {
-          this.get_string = data;
-          console.log(this.get_string)
+          this.getString = data;
+          console.log(this.getString);
       },
       error: error => {
           this.errorMessage = error.message;
           console.error('There was an error!', error);
       }
-  })
+  });
   }
 
 }
