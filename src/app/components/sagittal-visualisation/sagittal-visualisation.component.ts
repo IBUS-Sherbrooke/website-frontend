f33c6ef4e41ffbcdb4951cfff4948605659952f9
Michael <micha.samson@live.ca>
2021-10-07 15:52:59 -0400

    Clean up old code

--- src/app/components/sagittal-visualisation/sagittal-visualisation.component.ts
+++ src/app/components/sagittal-visualisation/sagittal-visualisation.component.ts
@@ -2,25 +2,8 @@ import { ViewChild, ElementRef, Component, OnInit } from '@angular/core';
 
 import { VtkManagerService } from '../../services/vtk-manager/vtk-manager.service';
 
-import vtkImageMapper from 'vtk.js/Sources/Rendering/Core/ImageMapper';
-import vtkImageSlice from 'vtk.js/Sources/Rendering/Core/ImageSlice';
-
-import vtkOpenGLRenderWindow from 'vtk.js/Sources/Rendering/OpenGL/RenderWindow';
-import vtkRenderWindow from 'vtk.js/Sources/Rendering/Core/RenderWindow';
-import vtkRenderWindowInteractor from 'vtk.js/Sources/Rendering/Core/RenderWindowInteractor';
-import vtkInteractorStyleImage from 'vtk.js/Sources/Interaction/Style/InteractorStyleImage';
-import vtkRenderer from 'vtk.js/Sources/Rendering/Core/Renderer';
-
-import vtkOrientationMarkerWidget from 'vtk.js/Sources/Interaction/Widgets/OrientationMarkerWidget';
-import vtkAxesActor from 'vtk.js/Sources/Rendering/Core/AxesActor';
-
-import vtkCornerAnnotation from 'vtk.js/Sources/Interaction/UI/CornerAnnotation';
-
-import Constants from 'vtk.js/Sources/Rendering/Core/ImageMapper/Constants';
-
 import { Subscription } from 'rxjs';
 
-const { SlicingMode } = Constants;
 
 @Component({
   selector: 'app-sagittal-visualisation',
@@ -74,20 +57,5 @@ export class SagittalVisualisationComponent implements OnInit {
       nw() { return `Sagittal`; }
     });
     this.viewProxy.resize();
-  }
-
-  orientationMarker() {
-    const axes = vtkAxesActor.newInstance();
-    const orientationWidget = vtkOrientationMarkerWidget.newInstance({
-      actor: axes,
-      interactor: this.interactor,
-    });
-    orientationWidget.setEnabled(true);
-    orientationWidget.setViewportCorner(
-      vtkOrientationMarkerWidget.Corners.BOTTOM_LEFT
-    );
-    orientationWidget.setViewportSize(0.15);
-    orientationWidget.setMinPixelSize(100);
-    orientationWidget.setMaxPixelSize(300);
   }
 }
--- src/app/components/transverse-visualisation/transverse-visualisation.component.ts
+++ src/app/components/transverse-visualisation/transverse-visualisation.component.ts
@@ -2,23 +2,7 @@ import { ViewChild, ElementRef, Component, OnInit } from '@angular/core';
 
 import { VtkManagerService } from '../../services/vtk-manager/vtk-manager.service';
 
-import vtkImageMapper from 'vtk.js/Sources/Rendering/Core/ImageMapper';
-import vtkImageSlice from 'vtk.js/Sources/Rendering/Core/ImageSlice';
-import vtkOpenGLRenderWindow from 'vtk.js/Sources/Rendering/OpenGL/RenderWindow';
-import vtkRenderWindow from 'vtk.js/Sources/Rendering/Core/RenderWindow';
-import vtkRenderWindowInteractor from 'vtk.js/Sources/Rendering/Core/RenderWindowInteractor';
-import vtkInteractorStyleImage from 'vtk.js/Sources/Interaction/Style/InteractorStyleImage';
-import vtkRenderer from 'vtk.js/Sources/Rendering/Core/Renderer';
-import Constants from 'vtk.js/Sources/Rendering/Core/ImageMapper/Constants';
-
-import vtkCornerAnnotation from 'vtk.js/Sources/Interaction/UI/CornerAnnotation';
-
-import vtkOrientationMarkerWidget from 'vtk.js/Sources/Interaction/Widgets/OrientationMarkerWidget';
-import vtkAxesActor from 'vtk.js/Sources/Rendering/Core/AxesActor';
-
 import { Subscription } from 'rxjs';
-
-const { SlicingMode } = Constants;
 
 @Component({
   selector: 'app-transverse-visualisation',
@@ -70,20 +54,5 @@ export class TransverseVisualisationComponent implements OnInit {
       nw() { return `Transverse`; }
     });
     this.viewProxy.resize();
-  }
-
-  orientationMarker() {
-    const axes = vtkAxesActor.newInstance();
-    const orientationWidget = vtkOrientationMarkerWidget.newInstance({
-      actor: axes,
-      interactor: this.interactor,
-    });
-    orientationWidget.setEnabled(true);
-    orientationWidget.setViewportCorner(
-      vtkOrientationMarkerWidget.Corners.BOTTOM_LEFT
-    );
-    orientationWidget.setViewportSize(0.15);
-    orientationWidget.setMinPixelSize(100);
-    orientationWidget.setMaxPixelSize(300);
   }
 }
